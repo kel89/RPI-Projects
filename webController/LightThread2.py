920c7d784c8b823012f3dc0a75732c4b030e5f5f
kel89 <kel89@cornell.edu>
2018-07-09 16:50:30 -0400

    Finished psuedo code

--- webController/LightThread2.py
+++ webController/LightThread2.py
@@ -41,6 +41,14 @@ app. This way, if a slider is adjusted, or a button pressed in the app, automati
 keep_jumping will be False, and the jumping will stop. (We can do something
 similar for the fading when we get that figured out)
 
+For fading, we first define an order list of colors, this is simply the color
+names in strings, that MUST be in the `COLORS` dictionary. We also need a 
+`fade` boolean switch. Similary, we need to add `keep_fading` arguments
+to the color and slider functions (Just as with the jumping). Now thinking
+about how the fading function will work, inside the while loop we will call
+a function that fades between two colors. This fade_between function will take
+two colors, and it will use the `speed` attribute; 
+
 """
 
 """
@@ -87,14 +95,18 @@ class LightThread(threading.Thread):
 		# Define the color dictionary: COLORS
 		# Note: we want to match this to the html file for the page
 
+		# Define the order colored names: ORDERED_COLORS
+		# this is used for the fading, to go from color to color in order
+
 		# define dimming constrol
 		# initiliaze to 0.75
 
-		# initliaze for jump
+		# initliaze for jump and fade
 		jump = False
+		fade = False
 		speed = 0.5 # play with this <----------
 
-	def set_red(self, r, keep_jumping=False):
+	def set_red(self, r, keep_jumping=False, keep_fading=False):
 		"""
 		Changes the red color -- Ultimate function
 
@@ -104,6 +116,8 @@ class LightThread(threading.Thread):
 		"""
 		# if not keep_jumping and jump is True
 		# set jump to false (flip the switch)
+
+		# do the same as above for fading
 
 
 		# save r as rSet (as still RGB number)
@@ -121,7 +135,7 @@ class LightThread(threading.Thread):
 		# however note that this time it will have a new dim
 		# Do this for all the colors
 
-	def to_color(self, color, keep_jumping=False):
+	def to_color(self, color, keep_jumping=False, keep_fading=False):
 		"""
 		Changes the lights to `color`, where `color` is a string
 		that *Must* be in COLORS
@@ -129,6 +143,8 @@ class LightThread(threading.Thread):
 		# if not keep_jumping AND jump is True
 		# i.e. we don't want to keep jumping but we currently are
 		# set jump to false
+
+		# do the same as above for fading
 
 		# Check that color is acceptable, if not...just print?
 		vals = self.COLORS[color]
@@ -145,6 +161,7 @@ class LightThread(threading.Thread):
 
 		# set swtich to on
 		jump = True
+		fade = False
 
 		# Start jumping loop
 		while jump:
@@ -153,7 +170,7 @@ class LightThread(threading.Thread):
 			# wait according to the speed timer <-------------- requires tweaking
 
 
-	def stop_jump(self):
+	def stop_jump_and_fade(self):
 		"""
 		Stops the jumping -- to be called when the button is explicitly
 		hit to stop the jumping
@@ -163,6 +180,7 @@ class LightThread(threading.Thread):
 
 		# flip the jump switch
 		jump = False 
+		fade = False
 
 		# Now get the current color, in RGB form and return it as a dictionary
 		# so that it can be sent JQURY style to update the sliders
@@ -175,7 +193,74 @@ class LightThread(threading.Thread):
 		# Note, this will require the html/js to be updated so that the jump
 		# button is bound to a getJSON
 
-	NOW THING ABOUT FADING
+	def fade_between(c1, c2):
+		"""
+		Takes in c1 and c2, which are strings, these must correspond to colors
+		in the COLORS dictionary, i.e. COLORS[c1] will return a list of 
+		three elements for [R,G,B].
+		Makes small adjustments to fade gently between these two colors, 
+		starting at c1, and moving to c2. It finds the max difference in R,B,G
+		and adjusts to take one step for the max
+		"""
+
+		# ensure at correct starting point
+		self.to_color(c1)
+
+		# find max number of steps
+		r_steps = self.COLORS[c2][0] - self.COLORS[c1][0]
+		g_steps = self.COLORS[c2][1] - self.COLORS[c1][1]
+		b_steps = self.COLORS[c2][2] - self.COLORS[c1][2]
+		max_steps = max(r_steps, g_steps, b_steps)
+
+		# get time between each step
+		time_between = (10/max_steps)*(2*self.speed) # gives 10 seconds per fade
+		# when speed = 0.5. Therefore can tweak 10 and 2 in tuning for looks
+
+		# set step sizes for each color
+		r_delta = r_steps/max_steps
+		g_delta = g_steps/max_steps
+		b_delta = b_steps/max_steps
+
+		# Start the fade loop
+		i = 0
+		while i < max_steps:
+			i += 1 # increment loop
+
+			# change color
+			self.set_red(self.rSet + r_delta, keep_fading=True)
+			self.set_green(self.gSet + g_delta, keep_fading=True)
+			self.set_blue(self.bSet + b_delta, keep_fading=True)
+
+			# pause
+			time.sleep(time_between)
+
+
+	def start_fade(self):
+		"""
+		Starts fading between colors
+		-------
+		Update this when done
+		"""
+
+		# flip fade switch
+		self.fade = True
+		self.jump = False
+
+		# Create loops that, while self.fade is True we iterate through 
+		# self.ORDERED_COLORS
+		num_colors = len(self.ORDERED_COLORS)
+		i = 0
+		while self.fade:
+			# choose colors
+			c1 = self.ORDERED_COLORS[i]
+			c2 = self.ORDERD_COLORS[i+1]
+
+			# fade between them
+			self.fade_between(c1, c2)
+
+			# increment loop
+			i = (i + 1)%(num_colors-2) # -2 so that we don't get error defining c2
+
 
 
 
